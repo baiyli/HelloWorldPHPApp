8f21a763685c967dfb90d710d54e7333c9c9de69
Baiyang Li <baiyanl@amazon.com>
2022-10-09 22:50:13 -0700

    Hello World PHP8.1 App

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>First PHP App</title></head>
+<body>
+<?php
+    print("Current PHP version=");
+    print(phpversion());
+    print("<br>");
+
+    print("Hello World!");
+    print("<br>");
+?>
+</body>
+</html>
